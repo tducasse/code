f97cb71d91b28a19088cf04e9932fc3965027b62
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2021-10-24 00:15:22 +1100

    Add peerjs and code sync

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import Peer from "peerjs";
 import Console from "./Console";
 import Editor from "./Editor";
 
 function App() {
   const [code, setCode] = useState("");
+  const [sessionId, setSessionId] = useState("");
+  const [peer, setPeer] = useState(null);
+  const [peerId, setPeerId] = useState("");
+  const [connection, setConnection] = useState(null);
 
   const run = () => {
     const scriptUrl = URL.createObjectURL(
@@ -31,14 +36,87 @@ function App() {
     iframeHead.appendChild(script);
   };
 
+  const joinSession = () => {
+    if (!sessionId || !peerId) return;
+    setConnection(peer.connect(sessionId));
+  };
+
+  const addConnectionListeners = useCallback(
+    (conn, sendCode = false) => {
+      conn.on("data", (data) => {
+        setCode(data);
+      });
+      conn.on("open", () => {
+        // eslint-disable-next-line no-console
+        console.log(`connection established`);
+        if (sendCode) {
+          conn.send(code);
+        }
+      });
+    },
+    [code]
+  );
+
+  useEffect(() => {
+    if (connection) {
+      addConnectionListeners(connection);
+    }
+  }, [addConnectionListeners, connection]);
+
+  useEffect(() => {
+    if (peer) {
+      peer.on("open", setPeerId);
+      if (peerId) {
+        peer.on("connection", (conn) => {
+          setSessionId(conn.peer);
+          setConnection(conn);
+          addConnectionListeners(conn, true);
+        });
+      }
+    }
+  }, [addConnectionListeners, peer, peerId]);
+
+  useEffect(() => {
+    setPeer(new Peer());
+  }, []);
+
+  const updateCode = (newCode) => {
+    setCode(newCode);
+    if (connection) {
+      connection.send(newCode);
+    }
+  };
+
   return (
     <div>
-      <button onClick={run} type="button">
-        Run
-      </button>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <button onClick={run} type="button">
+          Run
+        </button>
+        {peerId ? <div>Your id is {peerId}</div> : <div>Loading...</div>}
+
+        {peerId && (
+          <>
+            {connection ? (
+              <div>Connected to {sessionId}</div>
+            ) : (
+              <>
+                <input
+                  value={sessionId}
+                  onChange={(e) => setSessionId(e.target.value)}
+                  placeholder="session id"
+                />
+                <button onClick={joinSession} type="button">
+                  Join session
+                </button>
+              </>
+            )}
+          </>
+        )}
+      </div>
       <div style={{ display: "flex", flexDirection: "row" }}>
         <div style={{ flex: 1, height: "90vh" }}>
-          <Editor onChange={setCode} />
+          <Editor onChange={updateCode} value={code} />
         </div>
         <div style={{ flex: 1, height: "90vh", backgroundColor: "#353535" }}>
           <Console source={window.frames.code} />
--- src/Editor/Editor.js
+++ src/Editor/Editor.js
@@ -1,21 +1,24 @@
 import MonacoEditor from "@monaco-editor/react";
 import PropTypes from "prop-types";
 
-const Editor = ({ onChange }) => {
+const Editor = ({ value, onChange }) => {
   return (
     <MonacoEditor
       defaultLanguage="javascript"
       theme="vs-dark"
       onChange={onChange}
+      value={value}
     />
   );
 };
 
 Editor.propTypes = {
   onChange: PropTypes.func,
+  value: PropTypes.string,
 };
 Editor.defaultProps = {
   onChange: () => {},
+  value: "",
 };
 
 export default Editor;
